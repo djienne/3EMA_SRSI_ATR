8fc7967f8880ce946bd821f68fa4dc9b62f609c7
djienne <pofchewbacca@gmail.com>
2022-10-12 14:55:08 +0200

    updated

--- commands_deploy_fast_hyperopt.sh
+++ commands_deploy_fast_hyperopt.sh
@@ -6,5 +6,6 @@ git checkout stable
 ./setup.sh -i
 source ./.env/bin/activate
 cd ..
-git clone git@github.com:djienne/3EMA_SRSI_ATR.git
+cd 3EMA_SRSI_ATR/
+git clone https://github.com/djienne/3EMA_SRSI_ATR.git
 freqtrade hyperopt --strategy EMA3SRSI --timeframe 5m --config ./user_data/config_backtest.json --hyperopt-loss SortinoHyperOptLossDaily --analyze-per-epoch --min-trades 500 --timerange 20170817- --spaces buy -j -1 -e 3000
